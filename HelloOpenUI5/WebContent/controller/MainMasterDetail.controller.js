45a9993d039488ebb70c0993be76f3639844d742
kjumybit <hagen.kroeber@kjumybit.de>
2017-04-23 15:15:28 +0200

    Squashed commit of the following:
    commit c05e8765ecd18b45bdbc50dd8c6b7c861ebe6b67
    Author: kjumybit <hagen.kroeber@kjumybit.de>
    Date:   Sun Apr 23 15:11:19 2017 +0200
    
    	BugFix Manifest: Routes have unique "name" values
    
    commit a04dff0ffc0395628da833e2790a5ab7bbd7dea0
    Author: kjumybit <hagen.kroeber@kjumybit.de>
    Date:   Tue Apr 11 00:29:54 2017 +0200
    
    	Templates: Simple Page, Semantic FullScreen / Master Detail Page

--- HelloOpenUI5/WebContent/controller/MainMasterDetail.controller.js
+++ HelloOpenUI5/WebContent/controller/MainMasterDetail.controller.js
@@ -3,7 +3,7 @@ sap.ui.define([
 ], function(Controller) {
 	"use strict";
 
-	return Controller.extend("com.test.helloworld.controller.View1", {
+	return Controller.extend("com.test.helloworld.controller.MainMasterDetail", {
 
 	/**
 	* Called when a controller is instantiated and its View controls (if available) are already created.
